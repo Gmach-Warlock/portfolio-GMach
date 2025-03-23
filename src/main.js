3b007a0389565c6a1b0487d89d2620f0cd0e5af4
Gary Yamashita <garyjyam@gmail.com>
2025-03-22 17:42:03 -0700

    cta buttons made

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+// setup the counter
 import { setupCounter } from './counter.js'
-
-
-
 setupCounter(document.querySelector('#counter'))
+
+
+// create variables for my hidden sections
+
+let portfolioSection = document.getElementsByClassName('portfolio');
+let aboutSection = document.getElementsByClassName('about');
+let contactSection = document.getElementsByClassName('contact');
+
+
+
+// function to reveal portfolio section when the button is clicked
+
