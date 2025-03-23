4daa5e66e8dbc9a8471c2556d0913187a790e542
Gary Yamashita <garyjyam@gmail.com>
2025-03-22 20:36:13 -0700

    working on interactivity

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-// setup the counter
-import { setupCounter } from './counter.js'
+// setup the counter 
+/*import { setupCounter } from './counter.js'
 setupCounter(document.querySelector('#counter'))
+*/
 
 
-// create variables for my hidden sections
-
-let portfolioSection = document.getElementsByClassName('portfolio');
-let aboutSection = document.getElementsByClassName('about');
-let contactSection = document.getElementsByClassName('contact');
-
-
-
-// function to reveal portfolio section when the button is clicked
 
---
+++ src/script.js
@@ -0,0 +1,15 @@
+// create variables for my hidden sections
+
+let portfolioSection = document.getElementById('portfolio');
+let aboutSection = document.getElementById('about');
+let contactSection = document.getElementById('contact');
+
+
+
+// function to reveal portfolio section when the button is clicked
+
+const displayNone = () => {
+    portfolioSection.style.display = 'none';
+}
+
+document.body.display = 'none';
